f81294e3229c57b8c55fdc2ddbfaded60949a834
Yasin Sancaktutan <sancaktutanyasin@gmail.com>
2017-08-07 15:12:25 +0300

    first VCS test with PyCharm

--- music/views.py
+++ music/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
-
-
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
 
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('')
+
+    # html = ''
+    #
+    # for album in all_albums:
+    #     url = '/music/' + str(album.id) + '/'
+    #     html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse('')
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
